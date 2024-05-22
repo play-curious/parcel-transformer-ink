5ab74c3ef88d71c851defa93736f78fc6e9583bc
Jesse Himmelstein <jesse@heavenstone.net>
2024-05-22 14:42:45 +0200

    Added support for loading files

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const { Transformer } = require("@parcel/plugin");
 const { Compiler } = require("inkjs");
+const {
+  PosixFileHandler,
+} = require("inkjs/compiler/FileHandler/PosixFileHandler");
 
 module.exports = new Transformer({
   async transform({ asset, config }) {
@@ -9,7 +12,11 @@ module.exports = new Transformer({
     const source = await asset.getCode();
 
     // Compile it
-    const compiler = new Compiler(source);
+    const fileHandler = new PosixFileHandler();
+    const errorHandler = (message, errorType) => {
+      console.error("Ink File Handler: " + message + "\n");
+    };
+    const compiler = new Compiler(source, { fileHandler, errorHandler });
     const result = compiler.Compile().ToJson();
 
     // Update the asset and return it
